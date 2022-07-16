b5be81b3cdc9e633502778d306945bfeb9ef993d
DAKSHAY111 <86140759+DAKSHAY111@users.noreply.github.com>
2022-07-17 00:20:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router,Route,Switch} from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import DashBoard from './pages/Dashboard'
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import DashBoard from "./pages/Dashboard";
+import MainPage from "./pages/MainPage";
 
 const App = () => {
-	return (
-		
-		<Router>
-        
-        <Switch>
-          <Route exact path="/">
-          <Register/>
-           
-          </Route>
-          <Route exact path="/Login">
-            <Login/>
-          </Route>
-          <Route path="/dashboard">
-            <DashBoard />
-          </Route>
-        </Switch>
-        
-      </Router>
-		
-	)
-}
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Register />
+        </Route>
+        <Route exact path="/Login">
+          <Login />
+        </Route>
+        <Route path="/dashboard">
+          <DashBoard />
+        </Route>
+        <Route path="/Posts">
+          <MainPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
