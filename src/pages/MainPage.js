edc3684f4b823db56bbbc888ef4843d537fe90e4
DAKSHAY111 <86140759+DAKSHAY111@users.noreply.github.com>
2022-07-17 14:50:11 +0530

    Add files via upload

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from "react";
 const MainPage = () => {
   const [post, setPost] = useState("");
   useEffect(() => {
+    loadData();
+  }, []);
+  const loadData = async () => {
+    // const token = "Bearer " + localStorage.getItem("token");
     const token = "Bearer " + localStorage.getItem("token");
     if (token) {
       const getPost = async () => {
@@ -14,6 +18,7 @@ const MainPage = () => {
               "Content-Type": "application/json",
               authorization: token,
             },
+            
           }
         );
         const data = await response.json();
@@ -30,26 +35,35 @@ const MainPage = () => {
       alert("You are not signed in login first");
       window.location.href = "/login";
     }
-  }, []);
+  };
 
   return (
     <div>
-            <h2 style={{textAlign: "center"}}>Card Example</h2>
-            <div className="w3-container" style={{ display: "flex",flexWrap: "wrap", margin: "10px auto" }}>
-                {post.posts.map((ele,ind) => (
-                    <div className="w3-card-4" style={{ display: "flex",minWidth: "20%" ,flexDirection:"column", margin:"20px",borderRadius:"10px"}}>
-                        <header className="w3-container w3-light-grey">
-                            <h3 style={{textAlign: "center"}}>{ele.subject}</h3><hr/>
-                        </header>
-                        <div className="w3-container">
-                            <p>Id: {ele.id}</p>
-                            <p>Age: {ele.age}</p>
-                            <p>Position: {ele.position}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
+      <h2>Post Topic</h2>
+      <h1>HELLO WORLD</h1>
+
+      {post &&
+        post.posts.map((item) => (
+          <div>
+            <h1>{item.topic}</h1>
+            <h1>{item.subject}</h1>
+            {/* <h1>{item.photos}</h1> */}
+            {/* {item.photos.length > 0 && item.photos.map((pho) => <h2>{pho}</h2>)} */}
+            {/* {item.photos.length > 0 &&
+              item.photos.map((pho) => (
+                <img src="client/public/images/post/tour-62ccf9f27bed7c0570768714-1658027360973-1.jpeg" />
+              ))} */}
+          </div>
+        ))}
+      {/* {post.posts.map((data) => (
+        <h2>{data.subject}</h2>
+      ))} */}
+
+      {/* {post.posts.map((data) => (<>)} */}
+
+      <img src="images/post/{post.photos[0]}" alt="here" srcset="" />
+      {/* <h2>{post.message}</h2> */}
+    </div>
   );
 };
 
