a0b960d35d3869200157f6be130291c93da6bbc5
DAKSHAY111 <86140759+DAKSHAY111@users.noreply.github.com>
2022-07-17 12:24:09 +0530

    Add files via upload

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from "react";
 const MainPage = () => {
   const [post, setPost] = useState("");
   useEffect(() => {
+    loadData();
+  }, []);
+  const loadData = async () => {
+    // const token = "Bearer " + localStorage.getItem("token");
     const token = "Bearer " + localStorage.getItem("token");
     if (token) {
       const getPost = async () => {
@@ -30,18 +34,40 @@ const MainPage = () => {
       alert("You are not signed in login first");
       window.location.href = "/login";
     }
-  }, []);
+  };
 
   return (
     <div>
       <h2>Post Topic</h2>
-      {/* <h1>HELLO WORLD</h1>
-      {post.posts.map((data) => (
+      <h1>HELLO WORLD</h1>
+
+      {post &&
+        post.posts.map((item) => (
+          <div>
+            <h1>{item.topic}</h1>
+            <h1>{item.subject}</h1>
+            {/* <h1>{item.photos}</h1> */}
+            {/* {item.photos.length > 0 && item.photos.map((pho) => <h2>{pho}</h2>)} */}
+            {/* {item.photos.length > 0 &&
+              item.photos.map((pho) => (
+                <img src="client/public/images/post/tour-62ccf9f27bed7c0570768714-1658027360973-1.jpeg" />
+              ))} */}
+          </div>
+        ))}
+      {/* {post.posts.map((data) => (
         <h2>{data.subject}</h2>
       ))} */}
-      {post.posts.map((ele) => {
-        return <h2>{ele.subject}</h2>;
-      })}
+
+      {/* {post.posts.map((data) => (<>)} */}
+
+      <img
+        src={
+          window.location.origin +
+          "tour-62ccf9f27bed7c0570768714-1658027360973-1.jpg"
+        }
+        alt="here"
+        srcset=""
+      />
       {/* <h2>{post.message}</h2> */}
     </div>
   );
